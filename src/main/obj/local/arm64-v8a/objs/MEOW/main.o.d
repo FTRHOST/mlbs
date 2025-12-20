46293ef5d1994e5c4b3291673c2e4af41451477a
Muhammad Fathir Al Faruq <fathironmy4@gmail.com>
2025-12-20 22:33:10 +0700

    first commit

---
+++ src/main/obj/local/arm64-v8a/objs/MEOW/main.o.d
@@ -0,0 +1,54 @@
+/home/petwir/StudioProjects/imgc++ui/src/main/obj/local/arm64-v8a/objs/MEOW/main.o: \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/main.cpp \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/xdl/xdl.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Hook/Dobby/dobby.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/Include.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/Utils.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Utils/jniStuff.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Utils/Unity/ByNameModding/Tools.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Utils/Unity/ByNameModding/Includes.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Utils/Unity/ByNameModding/fake_dlfcn.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Utils/Unity/ByNameModding/Il2Cpp.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Utils/MonoString.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/Toggle.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/imgui_internal.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/imgui.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/imconfig.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/backends/imgui_impl_opengl3.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/backends/imgui_impl_android.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/font/Roboto-Regular.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/Icon.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/Iconcpp.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Utils/Unity/Vector3.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/Il2Cpp/il2cpp_dump.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Hook/KittyMemory/KittyMemory.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Hook/KittyMemory/MemoryPatch.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Hook/KittyMemory/KittyScanner.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Hook/KittyMemory/KittyUtils.h \
+  /home/petwir/StudioProjects/imgc++ui/src/main/jni/obfuscate.h
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/xdl/xdl.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Hook/Dobby/dobby.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/Include.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/Utils.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Utils/jniStuff.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Utils/Unity/ByNameModding/Tools.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Utils/Unity/ByNameModding/Includes.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Utils/Unity/ByNameModding/fake_dlfcn.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Utils/Unity/ByNameModding/Il2Cpp.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Utils/MonoString.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/Toggle.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/imgui_internal.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/imgui.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/imconfig.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/backends/imgui_impl_opengl3.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/backends/imgui_impl_android.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/font/Roboto-Regular.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/Icon.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/ImGui/Iconcpp.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Utils/Unity/Vector3.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/Il2Cpp/il2cpp_dump.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Hook/KittyMemory/KittyMemory.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Hook/KittyMemory/MemoryPatch.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Hook/KittyMemory/KittyScanner.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/include/Hook/KittyMemory/KittyUtils.h:
+/home/petwir/StudioProjects/imgc++ui/src/main/jni/obfuscate.h:
